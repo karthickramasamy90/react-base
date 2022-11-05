fe12ab95e3e88ca05620077f33bb4fa9708362f7
karthickramasamy90 <karthick.cse034@gmail.com>
2022-11-05 22:41:40 +0530

    Added products list

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ import Dashboard from './Dashboard';
 import PropsDrilling from './PropsDrilling';
 import PropsNonDrilling from "./PropsNonDrilling";
 import LocalStorage from "./LocalStorage";
+import Game from "./Game";
+import Products from './Products';
 
 function App() {
   return (
@@ -23,6 +25,8 @@ function App() {
           <Route exact path='/propsdrilling' element={<PropsDrilling />} />
           <Route exact path='/propsnondrilling' element={<PropsNonDrilling />} />
           <Route exact path="/localstorage" element={<LocalStorage />} />
+          <Route exact path="/games" element={<Game />} />
+          <Route exact path="/products" element={<Products />} />
         </Routes>
       </Router>
     </div>
